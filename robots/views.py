4f812da680c19b7ce438eb31363095b688596d14
Sergey Kovalskiy <Sergey.Kovalskiy@getrealprice.ru>
2023-09-26 08:17:27 +0300

    add create_robot

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
+from json import JSONDecodeError
+from django.http import HttpResponse
+from robots.models import Robot
 
-# Create your views here.
+
+def create_robot(request):
+    try:
+        data = json.loads(request.body)
+        print(type(data))
+    except JSONDecodeError:
+        return HttpResponse('Данные отсвутствуют или не верны')
+    field_names = [field.name for field in Robot._meta.get_fields()]
+
+    for field_name in field_names[2:]:
+        json_key = data.get(field_name)
+        if not json_key:
+            return HttpResponse(f'Ключ {field_name} отсутствует в JSON')
+
+    serial = f'{data["model"]}-{data["version"]}'
+    data['serial'] = serial
+
+    Robot.objects.create(**data)
+
+    return HttpResponse('Запись о произведенном роботе, добавлена в базу')
